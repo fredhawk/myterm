72f24794eb1c609251f2c41187df05aaa08e4484
Fred Hawk <fredrichawk@gmail.com>
2025-08-27 21:33:32 +0200

    Loading MyPrompt.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Start with checking so git and curl are installed. If they are not then install them.
+
+# Helpers for package managers
+
+# Check distribution
+
+# Install software
+
+# Install MyPrompt
+git clone --depth=1 https://github.com/fredhawk/myprompt.git && cd myprompt && bash ./setup.sh
+
+# Finish
