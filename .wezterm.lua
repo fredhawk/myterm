d1b4236bbaba31f133c95e1c2825116730cb283e
Fred Hawk <fredrichawk@gmail.com>
2025-08-30 14:38:04 +0200

    Added Wezterm config.

---
+++ .wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.default_prog('/bin/bash')
+config.color_scheme = 'Tokyo Night Storm'
+config.enable_tab_bar = false
+config.font = wezterm.font("Fira Code")
+config.font_size = 12.5
+config.line_height = 1.2
+config.window_background_opacity = 0.8
+-- config.enable_wayland = false
+
+return config
